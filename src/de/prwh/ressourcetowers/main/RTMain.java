79d11868539257159f7c5ecd2dd041a6dcf5f9af
dragonisser <dragonisser@googlemail.com>
2016-05-15 20:20:01 +0200

    Added plugin.yml and main file

---
+++ src/de/prwh/ressourcetowers/main/RTMain.java
@@ -0,0 +1,14 @@
+package de.prwh.ressourcetowers.main;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RTMain extends JavaPlugin {
+
+	public void onEnable() {
+
+	}
+
+	public void onDisable() {
+
+	}
+}
